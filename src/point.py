6f818fa53058b92970e88ae7b4e0e818afc9948b
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-11-20 00:56:20 -0200

    Modifying Point interface

--- src/point.py
+++ src/point.py
@@ -4,24 +4,26 @@
 class Point(tuple):
     """ Represents a 2D point. Inherits built-in tuple """
 
-    def __new__(cls, x, y):
-        return tuple.__new__(cls, (int(x), int(y)))
+    def __new__(cls, id, x, y):
+        return tuple.__new__(cls, (int(id), int(x), int(y)))
 
-    def __init__(self, x, y):
-        super(Point, self).__init__(x, y)
+    def __init__(self, id, x, y):
+        super(Point, self).__init__(id, x, y)
+        self.id = id
         self.x = x
         self.y = y
 
     def __repr__(self):
-        return "Point(tuple) Object - (%s, %s)" % (self.x, self.y)
+        return "Point(tuple) Object - P%s=(%s, %s)" % (self.id, self.x, self.y)
 
 
 # Testing class
 if __name__ == "__main__":
 
-    point = Point(3, 8)
+    point = Point(0, 3, 8)
 
     print point
+    print "id = %s" % point.id
     print "x = %s" % point.x
     print "y = %s" % point.y
 
