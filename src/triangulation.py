ef12e31fedfbd5d482ab30999d91ab73698a16d8
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-11-20 20:44:24 -0200

    Added the process method and class test case

--- src/triangulation.py
+++ src/triangulation.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from point import Point
+from triangle import Triangle
 
 
 class Triangulation(object):
@@ -16,33 +17,58 @@ class Triangulation(object):
         elif not all(isinstance(point, Point) for point in points):
             raise TypeError("elements of list must be Point objects")
 
+        self.points = points
 
     # TODO: If the points come sorted then it is not necessary to calculate
-    # the Area 
-    def area(self, points):
+    # the Area
+    def area(self):
         """ Calculates the polygon area """
-        
+
         area = 0
-        n = len(points)
+        n = len(self.points)
         p, q = n-1, 0
-        
+
         while q < n:
-            area += points[p].x*points[q].y - points[q].x*points[p].y
+            area += self.points[p].x * self.points[q].y \
+                    - self.points[q].x * self.points[p].y
             q += 1
-            p=q
+            p = q
 
         return area*0.5
 
-    def process(self, points):
+    def process(self):
         """ Process and triangulates the polygon """
+        import ipdb
+        ipdb.set_trace()
+        result = []
 
-        # Minimum number of points to compose a triangle 
-        if len(points) < 3:
+        # Minimum number of points to compose a triangle
+        if len(self.points) < Triangle.MAX_POINTS:
             return False
 
-        # Counter Clockwise polygon TODO: Verify if is necessary. See def area
-        
+        n_points = len(self.points)
+        tmp_points = self.points[:]
+        i = n_points - 1
 
+        loop_control = 2 * n_points
+
+        while len(tmp_points) > Triangle.MAX_POINTS:
+
+            if loop_control <= 0:
+                return Exception("Bla")
+                
+
+            # Creates a Triangle object with three consecutives Point objects
+            triangle = Triangle(tmp_points[i % n_points],
+                                tmp_points[(i+1) % n_points],
+                                tmp_points[(i+2) % n_points])
+
+            if self.snip(tmp_points, triangle):
+                result.append(triangle)
+                tmp_points.remove(tmp_points[i+1])
+
+        del tmp_points
+        return result
 
     def is_inside(self, point, triangle):
         """ Verify if a given point belongs to a triangle inner area """
@@ -62,15 +88,44 @@ class Triangulation(object):
         return a_cross >= 0 and b_cross >= 0 and c_cross >= 0
 
     def snip(self, points, triangle):
-        
+        """Return true if the polygon can be snipped """
+
         oppos = (triangle.v.x - triangle.u.x) * (triangle.w.y - triangle.u.y)
         adjac = (triangle.v.y - triangle.u.y) * (triangle.w.x - triangle.u.x)
 
-        if EPSILON > (oppos - adjac):
+        if self.EPSILON > (oppos - adjac):
             return False
+
+        # verify if the point belongs to the triangle
+        for point in points:
+            if point not in triangle and self.is_inside(point, triangle):
+                return False
+        return False
 
 
 # Testing the class
 if __name__ == "__main__":
 
-   pass 
+    points = [Point(0, 0, 6),
+              Point(1, 0, 0),
+              Point(2, 3, 0),
+              Point(3, 4, 1),
+              Point(4, 6, 1),
+              Point(5, 8, 0),
+              Point(6, 12, 0),
+              Point(7, 13, 2),
+              Point(8, 8, 2),
+              Point(9, 8, 4),
+              Point(10, 11, 4),
+              Point(11, 11, 6),
+              Point(12, 6, 6),
+              Point(13, 4, 3),
+              Point(14, 2, 6)]
+
+    triangulation = Triangulation(points)
+
+    for triangle in triangulation.process():
+        print "Triangle %d => (%s, %s) (%s, %s) (%s, %s)" \
+              % (triangle.u.x, triangle.u.y,
+                 triangle.v.x, triangle.v.y,
+                 triangle.w.x, triangle.w.y)
