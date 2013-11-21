22a92093d04e48a4288c76c861bc00b659cea754
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-11-21 15:59:11 -0200

    Incomplete implementation of Coloring class

---
+++ src/coloring.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+class Coloring(object):
+
+    def __init__(self):
+        raise NotImplementedError("Come on dude, You need to write my code!")
+
+
+# Testing the class
+if __name__ == "__main__":
+
+    coloring = Coloring()
