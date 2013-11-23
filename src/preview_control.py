553702a67cfca7d8c5e53e9f900006b442a97c28
Gustavo Pantuza Coelho Pinto <gustavopantuza@gmail.com>
2013-11-23 19:52:59 -0200

    Modified the class to work entirely static

--- src/preview_control.py
+++ src/preview_control.py
@@ -2,44 +2,51 @@
 
 from preview import Preview
 
+
 class PreviewControl(object):
-    """ This class is a controler for preview. 
-    
+    """ 
+    This class is a controler for preview. 
     """
 
+    preview = None
+
     def __init__(self, painter_class = None):
-        self.preview = None
         self.painter_class = painter_class
 
-    def start(self, **args):
+    @staticmethod
+    def start(**args):
         '''
         Automatic create a default previewer object
         '''
-        self.preview = Preview(**args)
-        self.preview.start()
+        PreviewControl.preview = Preview(**args)
+        PreviewControl.preview.start()
 
-    def stop(self):
+    @staticmethod
+    def stop():
         '''
         Stop the default previewer
         '''
-        self.preview.stop()
+        PreviewControl.preview.stop()
 
-    def add_view(self, element, label=''):
+    @staticmethod
+    def add_view(element, label=''):
         '''
         Include a diagram into the default previewer
         '''
         #print "new_diagram call: label=%s" % label
-        self.preview.add_view(element, label)
+        PreviewControl.preview.add_view(element, label)
 
-    def in_state(self, state):
+    @staticmethod
+    def in_state(state):
         '''
         Checks the state of the default preview
         '''
-        if self.preview is None: 
+        if PreviewControl.preview is None: 
             return state == Preview.NONE
-        return state == self.preview.state
+        return state == PreviewControl.preview.state
 
-    def show(self, view, title = '', lines = 1, columns = 1):
+    @staticmethod
+    def show(view, title = '', lines = 1, columns = 1):
         preview = Preview(title = title, lines = lines, columns = columns,
                           painter_class = self.painter_class)
         preview.add_view(view, title)
@@ -47,4 +54,3 @@ class PreviewControl(object):
         preview.start()
         while preview.step():
             pass
-        
